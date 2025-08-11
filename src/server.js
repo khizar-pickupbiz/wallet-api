96e6d77ec4a981e38e9ae8af7e9e2d45f6de30f2
kodingKhizar <kodingkhizar@gmail.com>
2025-08-12 01:04:33 +0530

    initial commit

---
+++ src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import { initDB } from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+import transactionsRoutes from "./routes/transactionsRoutes.js"
+
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(rateLimiter)
+app.use(express.json());
+
+const PORT = process.env.PORT || 5001;
+
+
+
+app.use("/api/transaction",transactionsRoutes)
+// app.use("/api/products",transactionsRoutes)
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
+});
